688448a68370f66f78bf363994edebcaa3d51e61
ignaciovi <ignaciovaldelvira@PCLAP19.local>
2023-07-26 17:25:10 +0200

    add unique id, use functions, max iteration rules, filter out non important roles

--- get_experience_in_past_company.py
+++ get_experience_in_past_company.py
@@ -12,10 +12,9 @@
 
 from linkedinapi.linkedin_api import Linkedin
 from linkedinapi.linkedin_api.utils import helpers
-from numpy import random
 from time import sleep
 from google.cloud import storage
-
+from hashlib import sha256
 
 import datetime
 import calendar
@@ -23,6 +22,7 @@ import json
 import pandas as pd
 import numpy as np
 import os
+import random
 
 
 # In[ ]:
@@ -50,19 +50,58 @@ storage_client = storage.Client()
 bucket = storage_client.get_bucket("job-hunt-company-analyser")
 
 
+# ## Method to get array of seconds between iterations
+
+# In[ ]:
+
+
+def subtract_adjacent_elements(arr):
+    result = []
+    for i in range(len(arr) - 1):
+        result.append(arr[i+1] - arr[i])
+    return result
+
+
+# In[ ]:
+
+
+def get_random_seconds_between_iterations_array(number_of_iterations, number_of_hours_for_job):
+    number_of_seconds = number_of_hours_for_job * 60 * 60
+    seconds_array = sorted(random.sample(range(1, number_of_seconds), number_of_iterations))
+    
+    print(seconds_array)
+    seconds_diff_array = [0] + (subtract_adjacent_elements(seconds_array))
+    
+    return seconds_diff_array
+    
+
+
+# In[ ]:
+
+
+
+
+
 # # Get company based on name
 
 # In[ ]:
 
 
-company_to_look = 'Too good to go'
+
+
+
+# In[ ]:
+
+
+company_to_look = 'Odilo'
 
 
 # In[ ]:
 
 
 companies = api.get_companies(company_to_look)
-today = datetime.date.today()
+today_date = datetime.datetime.now()
+today = today_date.strftime("%Y-%m-%d %H:%M:%S")
 
 
 # In[ ]:
@@ -98,11 +137,16 @@ searched_company_location = searched_company['primarySubtitle'].split(' • ')[1
 # In[ ]:
 
 
+id_to_encode = searched_company_urn + today
+
+id_encoded = sha256(id_to_encode.encode('utf-8')).hexdigest()
+
 searched_company_dict = {
-    'company_urn': searched_company_urn,
-    'company_title': searched_company_title,
-    'company_industry': searched_company_industry,
-    'company_location': searched_company_location,
+    'id': id_encoded,
+    'urn': searched_company_urn,
+    'name': searched_company_title,
+    'industry': searched_company_industry,
+    'location': searched_company_location,
     'scraped_at': today,
 }
 
@@ -110,7 +154,13 @@ searched_company_dict = {
 # In[ ]:
 
 
-pd.DataFrame([searched_company_dict])
+df_company_searched = pd.DataFrame([searched_company_dict])
+
+
+# In[ ]:
+
+
+df_company_searched
 
 
 # In[ ]:
@@ -119,11 +169,13 @@ pd.DataFrame([searched_company_dict])
 # 76334424
 
 
+# ## Store to bucket
+
 # In[ ]:
 
 
 file_name_company_name = searched_company_title.replace(' ', '_').lower()
-file_name_date = today.strftime('%Y%m%d')
+file_name_date = today_date.strftime('%Y%m%d%H%M%S')
 
 
 # In[ ]:
@@ -135,8 +187,14 @@ file_name_company_searched = f'company_searched_{file_name_company_name}_{file_n
 # In[ ]:
 
 
+file_name_company_searched
+
+
+# In[ ]:
+
+
 bucket.blob(f'company_searched/{file_name_company_searched}.csv').upload_from_string(
-                    df_employees.to_csv(index=False), 'text/csv')
+                    df_company_searched.to_csv(index=False), 'text/csv')
 
 
 # # Get employees
@@ -144,15 +202,13 @@ bucket.blob(f'company_searched/{file_name_company_searched}.csv').upload_from_st
 # In[ ]:
 
 
-# TODO increase number of employees displayed playing with offset and count
-# employees = api.get_employees(searched_company_urn, offset=1, count=2)
-# employees = api.get_employees(3266, count=5)
-
-
-# In[ ]:
-
-
-# employees
+employees_experience_input = {
+    'role_keywords_searched': ['data', 'engineer', 'analyst', 'software', 'developer', 'fullstack', 
+                          'full stack', 'full-stack', 'backend'],
+    'threshold_number_of_roles_with_keywords_to_stop': 50,
+    'threshold_total_number_of_roles_to_stop': 200,
+    'threshold_number_of_iterations_to_stop': 20
+}
 
 
 # In[ ]:
@@ -160,37 +216,88 @@ bucket.blob(f'company_searched/{file_name_company_searched}.csv').upload_from_st
 
 # Find 50 employees that are not out of network
 
-employees_list = []
-for selected_offset in [0,50,100,150,200,250]:
-    print(selected_offset)
-    employees = api.get_employees(searched_company_urn, offset=selected_offset, count=50)
-    indexes_to_drop = []
+def get_employees_experience(employees_experience_input):
+    employees_all_list = []
+    employees_data_list = []
     
-    for idx, x in enumerate(employees):
-        if x['memberDistance'] == 'OUT_OF_NETWORK':
-            indexes_to_drop += [idx]
-    
-    employees = np.delete(employees, indexes_to_drop)
-    
-    employees_list += list(employees)
-    
-    employees_list = list({v['entityUrn']:v for v in employees_list}.values())
-    
-    if len(employees_list) >= 50:
-        break
+    seconds_diff_array = get_random_seconds_between_iterations_array(5, 1)
+
+    iterator = 0
+    while True:
+        sleeptime = random.uniform(30, 90)
+
+        selected_offset = 50 * iterator
+        print(f'Offset: {selected_offset}')
+        iterator += 1
+
+        employees = api.get_employees(searched_company_urn, offset=selected_offset, count=50)
+
+        if not bool(employees):
+            break
+      
+        indexes_to_drop = []
+        indexes_of_data = []
+
+        for idx, x in enumerate(employees):
+            if x['memberDistance'] == 'OUT_OF_NETWORK':
+                indexes_to_drop += [idx]
+            elif any([y in (x['primarySubtitle']).lower() for y in role_keywords_searched]):
+                indexes_of_data += [idx]
+
+        employees = np.array(employees)
+
+        employees_data = employees[indexes_of_data]
+
+        employees_all = np.delete(employees, indexes_to_drop)
+
+        employees_all_list += list(employees_all)
+        employees_data_list += list(employees_data)
+
+        employees_all_list = list({v['entityUrn']:v for v in employees_all_list}.values())
+        employees_data_list = list({v['entityUrn']:v for v in employees_data_list}.values())
+
+        # Find at least X data employees and Y total employees
+        # Set a maximum of iterations
+
+        is_max_iterations = employees_experience_input['threshold_number_of_iterations_to_stop'] == iterator
+        
+        is_over_threshold_keyword_employees = len(employees_data_list) > employees_experience_input['threshold_number_of_roles_with_keywords_to_stop']
+        is_over_threshold_all_employees = len(employees_all_list) > employees_experience_input['threshold_total_number_of_roles_to_stop']
+
+        is_final_iteration = is_max_iterations or (is_over_threshold_keyword_employees and is_over_threshold_all_employees)
+
+        if is_final_iteration:
+            break
+
+        sleep(sleeptime)
+        
+    return employees_all_list
+
     
 
 
 # In[ ]:
 
 
-employees_list = employees_list[0:50]
+# employees_data_list
 
 
 # In[ ]:
 
 
-pd.DataFrame(employees_list)
+# employees_all_list = employees_all_list[0:50]
+
+
+# In[ ]:
+
+
+employees_all_list = get_employees_experience(employees_experience_input)
+
+
+# In[ ]:
+
+
+pd.DataFrame(employees_all_list)
 
 
 # In[ ]:
@@ -204,7 +311,7 @@ pd.DataFrame(employees_list)
 # In[ ]:
 
 
-len(employees_list)
+len(employees_all_list)
 
 
 # In[ ]:
@@ -224,8 +331,13 @@ len(employees_list)
 
 employees_experience = []
 i = 0
-for employee in employees_list:
-    sleeptime = random.uniform(2, 15)
+            
+today_date = datetime.datetime.now()
+today = today_date.strftime("%Y-%m-%d %H:%M:%S")
+
+for employee in employees_all_list:
+    
+    sleeptime = random.uniform(30, 90)
     urn = employee['entityUrn'].split(':')[-1].split(',')[0]
     
     i += 1
@@ -235,14 +347,22 @@ for employee in employees_list:
 
     profile = api.get_profile(urn_id = urn)
     
+    
     if not bool(profile):
         continue
+        
+#     for education in profile['education']:
+        
     
     for experience in profile['experience']:
         
         try:
             
-            today = datetime.date.today()
+            is_intern = True if 'intern' in (experience['title']).lower() else False
+            
+            
+            if is_intern:
+                continue
 
             time_period = experience['timePeriod']
 
@@ -256,7 +376,7 @@ for employee in employees_list:
                 last_reference_year = None
                 last_reference_month = None
 
-                last_reference_date = today
+                last_reference_date = today_date.date()
 
             else:
                 last_reference_year = int(time_period['endDate']['year'])
@@ -264,7 +384,6 @@ for employee in employees_list:
                 last_reference_day = calendar.monthrange(last_reference_year, last_reference_month)[1]
 
                 last_reference_date = datetime.date(last_reference_year, last_reference_month, last_reference_day)
-
 
             months_in_company = 12 * (last_reference_date.year - start_date.year) + (last_reference_date.month - start_date.month)
 
@@ -274,8 +393,13 @@ for employee in employees_list:
             
             is_target_company = experience['companyName'] in searched_company_title
             
+            id_to_encode = urn + experience['title'] + experience['companyName'] + today
+        
+            id_encoded = sha256(id_to_encode.encode('utf-8')).hexdigest()
+            
             employees_experience.append({
-                'urn': urn,
+                'id': id_encoded,
+                'employee_urn': urn,
                 'title': experience['title'], 
                 'industry': experience['company']['industries'][0],
                 'company_name': experience['companyName'], 
@@ -291,11 +415,10 @@ for employee in employees_list:
                 'scraped_at': today
 
             })
-            
+
         except:
             continue
-    
-            
+
     sleep(sleeptime)
     
 
@@ -303,13 +426,50 @@ for employee in employees_list:
 # In[ ]:
 
 
-df_employees = pd.DataFrame(employees_experience)
+employees_experience
 
 
 # In[ ]:
 
 
-df_employees.head()
+# TODO add education
+# profile['education']
+
+
+# In[ ]:
+
+
+# employees_experience
+
+
+# In[ ]:
+
+
+df_employees_experience = pd.DataFrame(employees_experience)
+
+
+# In[ ]:
+
+
+df_employees_experience
+
+
+# In[ ]:
+
+
+# df_employees_experience[df_employees_experience['employee_urn'] == 'ACoAAB3ksgQBLsTVCfaFSdCGdVMzqVJHxuCDQy4']
+
+
+# In[ ]:
+
+
+# profile = api.get_profile(urn_id = 'ACoAAAPt8PQBiNyyY_-0Lcr198R9zbGOAOPdHwI')
+
+
+# In[ ]:
+
+
+# profile['education']
 
 
 # In[ ]:
@@ -321,19 +481,19 @@ df_employees.head()
 # In[ ]:
 
 
-
-
-
-# In[ ]:
-
-
-df_employees.query(f'company_name == \'{searched_company_title}\' and end_date == end_date').head()
+# df_employees_experience = df_employees_experience.rename(columns={'urn': 'employee_urn'})
 
 
 # In[ ]:
 
 
-group_months_in_company = df_employees.query(f'company_name == \'{searched_company_title}\' and end_date == end_date').groupby('months_in_company')['months_in_company'].count()
+df_employees_experience.query(f'company_name == \'{searched_company_title}\' and end_date == end_date')
+
+
+# In[ ]:
+
+
+group_months_in_company = df_employees_experience.query(f'company_name == \'{searched_company_title}\' and end_date == end_date').groupby('months_in_company')['months_in_company'].count()
 
 
 # In[ ]:
@@ -396,19 +556,13 @@ file_name_employees_experience
 df_employees.to_pickle(f'{file_name}.pickle')
 
 
-# In[ ]:
-
-
-
-
-
-# # Store in bucket
+# ## Store in bucket
 
 # In[ ]:
 
 
 bucket.blob(f'employees_experience/{file_name_employees_experience}.csv').upload_from_string(
-                    df_employees.to_csv(index=False), 'text/csv')
+                    df_employees_experience.to_csv(index=False), 'text/csv')
 
 
 # In[ ]:
